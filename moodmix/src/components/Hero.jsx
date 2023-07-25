2dc080a953d1572547db48593e76a486d236fc76
pkante21 <pranav.kante@newrcompany.com>
2023-07-25 00:01:12 -0400

    gradient text

--- moodmix/src/components/Hero.jsx
+++ moodmix/src/components/Hero.jsx
@@ -6,20 +6,20 @@ const Hero = () => {
   return (
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-[#00fd9a] font-bold p-2'>AI Based Recommendations</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Find Music Right For You</h1>
+            <p className='text-[#00fd9a] font-bold'>AI Based Recommendations</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-3'>Music Right For You</h1>
             <div className='flex-justify-center items-center'>
-                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-                    Im Looking For
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-2'>
+                    Im Looking For:
                 </p>
                 <Typed
-                className=' text-pink-300 md:text-5xl sm:text-4xl text-xl font-bold pl-2' 
-                strings={['A playlist while I drive at night', 'A song after I got heartbroken', 'Rage Music']} 
+                className=' text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-purple-600 md:text-4xl sm:text-3xl text-xl font-bold pl-2' 
+                strings={['playlists while I drive at night', 'a song after I got heartbroken', 'a playlist while I walk through NYC']} 
                 typeSpeed={80} 
                 backSpeed={70} 
                 loop/>
             </div>
-            <p className='md:text-2xl text-xl font-bold text-gray-500'>Powered by ChatGPT, Spotify, and Computer Vision. MoodMix revolutionizes how music is heard</p>
+            <p className='md:text-2xl text-xl font-bold text-gray-500 py-3'>Powered by ChatGPT, Spotify, and Computer Vision. MoodMix revolutionizes how music is heard</p>
             <Link to="/chat"><button className='bg-[#00fd9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-white'>Get Started Now</button></Link>
 
         </div>
